c712c50f6d025483c1bd2318addff4961f7ced04
nishantsingh604 <nishantrohilla28@gmail.com>
2025-07-13 23:28:58 +0530

    adding video components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,13 +4,23 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <main className='wrapper page'>
-      <Header title='All Videos' subHeader='Public Library'/>
-      <h1 className='text-2xl font-karla'>Welcome to PicScreen</h1>
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library" />
+      <h1 className="text-2xl font-karla">Welcome to PicScreen</h1>
 
-      <VideoCard/>
+      <VideoCard
+        id="1"
+        title="Snapchat message"
+        thumbnail="/assets/samples/thumbnail (1).png"
+      createdAt="2023-10-01T12:00:00Z"
+      userImg="/assets/images/jason.png"
+      username="jason"
+      views={10}
+      visibility="public"
+      duration={156}
+      />
     </main>
-  )
+  );
 }
 
 export default Page
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const VideoCard = () => {
+const VideoCard = ({id,title,thumbnail,userImg,username,createdAt,views,visibility,duration}:VideoCardProps) => {
   return (
-    <div>VideoCard</div>
+    <Link href={`/video/${id}`} className="video-card">
+      <Image src={thumbnail} alt="thumbnail" width={290} height={160} className='thumbnail'/>
+      <article></article>
+    </Link>
   )
 }
 
